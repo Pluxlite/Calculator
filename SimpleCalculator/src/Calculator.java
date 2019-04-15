94e34a5458c3ccf4f1e77c1587fa94995dba7ec5
AnAmorphous <stefnyhanley96@hotmail.com>
2019-04-15 15:43:35 +0100

    Add files via upload

---
+++ SimpleCalculator/src/Calculator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+//Scanner = for keyboard use
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in); // get input from keyboard 
+		
+		int option = 0;
+		int num1 = 0;
+		int num2 = 0;
+		
+		System.out.println("input number for calculation option\n1: Addition\n2: Subtration\n3: Mutliply\n4: Divide");
+		option = input.nextInt();// setting option from the switch
+		
+		System.out.println("Enter first number");
+		num1 = input.nextInt();// getting number from keyboard
+		
+		System.out.println("Enter second number");
+		num2 = input.nextInt();
+		
+		switch(option){
+		case 1: System.out.println(num1 + num2);
+		break;
+		case 2: System.out.println(num1 - num2);
+		break;
+		case 3: System.out.println(num1 * num2);
+		break;
+		case 4: System.out.println(num1 / num2);
+		}
+		
+		}
+
+}
+		
+		
+		
+		//System.out.println("First number please: ");
+		//int Num1 = input.nextInt(); // grabbing num1 from keyboard
+		
+		//System.out.println("Second number please: ");
+		//int Num2 = input.nextInt(); // grabbing num2 from keyboard
+		
+		//int answer = Num1 + Num2; // setting the addition sum
+		
+		//System.out.println("Your answer is : " + answer); // printing answer
+		
+		
+		
+		
+		
+	
